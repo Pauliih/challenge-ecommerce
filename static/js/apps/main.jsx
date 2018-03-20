80a43e0e57356e17f421eea3751d5b0266a73357
Pauliih <pau.campos89@gmail.com>
2018-03-20 11:33:40 -0300

    total button example

--- static/js/apps/main.jsx
+++ static/js/apps/main.jsx
@@ -1,6 +1,73 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
 
-const App = ({ message }) => <div>{ message }</div>;
+// const App = ({ message }) => 
+//   <div>{ message }</div>;
 
-ReactDOM.render(<App message="hello world" />, document.getElementById('app'));
+// React component for Total Button
+class Total extends Component {
+  render() {
+    const { value, onIncreaseClick } = this.props
+    return (
+      <div>
+        <button onClick={onIncreaseClick}>Total: <span>{value}</span></button>
+      </div>
+    )
+  }
+}
+
+Total.propTypes = {
+  value: PropTypes.number.isRequired,
+  onIncreaseClick: PropTypes.func.isRequired
+}
+
+// Action
+const increaseAction = { type: 'increase' }
+
+// Reducer
+function counter(state = { count: 0 }, action) {
+  const count = state.count
+  switch (action.type) {
+    case 'increase':
+      return { count: count + 1 }
+    default:
+      return state
+  }
+}
+
+// Store
+const store = createStore(counter)
+
+// Map Redux state to component props
+function mapStateToProps(state) {
+  return {
+    value: state.count
+  }
+}
+
+// Map Redux actions to component props
+function mapDispatchToProps(dispatch) {
+  return {
+    onIncreaseClick: () => dispatch(increaseAction)
+  }
+}
+
+// Connected Component
+const App = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Total)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+)
+
+// ReactDOM.render(
+//   <App message="Hello world" />, document.getElementById('app')
+// );
